3fbaa7145ef0f1ce8251aa54a26e8f7f4f3f9590
Ekroop05 <ekroopsingh2005@gmail.com>
2026-03-04 18:48:10 +0530

    switch to upstash rest api

--- redisClient.js
+++ redisClient.js
@@ -1,16 +1,8 @@
-const redis = require("redis");
+const { Redis } = require("@upstash/redis");
 
-const client = redis.createClient({
-  url: process.env.REDIS_URL
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
 
-client.on("error", (err) => {
-  console.log("Redis Error:", err);
-});
-
-(async () => {
-  await client.connect();
-  console.log("Redis Connected");
-})();
-
-module.exports = client;
+module.exports = redis;
